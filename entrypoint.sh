495a5a87c3dd8bf6d2b85f8ad62057cef4c306b9
Ivris Raymond <theivris@gmail.com>
2023-03-02 21:12:38 -0600

    merge time

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 echo "IN ENTRYPOINT"
 ./retrieve_data.sh
 
-./create-tiles.sh ./data/*.tif 10 10
+./create-tiles.sh ./data/USGS_13_n37w094_20230117.tif 10 10
+./create-tiles.sh ./data/USGS_13_n37w095_20221102.tif 10 10
+./create-tiles.sh ./data/USGS_13_n36w094_20180726.tif 10 10
+./create-tiles.sh ./data/USGS_13_n36w095_20190405.tif 10 10
 
 exec "$@"
